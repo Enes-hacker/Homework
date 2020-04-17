f8d37cfd413e6776f02e796dc2626cf787e26154
Enes-hacker <63426211+Enes-hacker@users.noreply.github.com>
2020-04-17 10:55:03 +0300

    Add files via upload

---
+++ Hello World!/src/com/enes/Main.java
@@ -0,0 +1,17 @@
+package com.enes;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here}
+
+        int rectangle;
+        int a = 2;
+        int b = 6;
+        int S = a * b;
+        int Perimeter = 2*a + 2*b;
+        System.out.println ("S of rectangle is equal:" + S);
+        System.out.println ("Perimeter of rectangle is equal to:"+ Perimeter);
+
+    }
+}
